6058aa0be4c863bdde4636ecace1d2c8f8dc896d
Lin-han-bin <a0989891296@gmail.com>
2015-03-18 12:05:40 +0800

    first build

---
+++ ksana.js
@@ -0,0 +1,45 @@
+jsonp_handler({
+ "version": "1",
+ "build": 8,
+ "title": "todolist",
+ "minruntime": 1,
+ "baseurl": "http://rawgit.com/a0989891296/todolist/master/",
+ "description": "",
+ "browserify": {
+  "external": [
+   "react",
+   "bootstrap"
+  ]
+ },
+ "files": [
+  "index.html",
+  "index.css",
+  "react-bundle.js",
+  "bundle.js",
+  "jquery.js",
+  "ksana.js",
+  "bootstrap.min.js",
+  "bootstrap.min.css"
+ ],
+ "filesizes": [
+  547,
+  116,
+  652507,
+  97572,
+  277980,
+  777,
+  35607,
+  113502
+ ],
+ "filedates": [
+  "2015-03-18T03:09:27.185Z",
+  "2015-03-18T03:51:51.040Z",
+  "2015-03-18T03:09:27.261Z",
+  "2015-03-18T04:04:40.858Z",
+  "2015-03-18T03:09:27.212Z",
+  "2015-03-18T04:04:05.339Z",
+  "2015-03-18T03:09:27.178Z",
+  "2015-03-18T03:09:27.167Z"
+ ],
+ "date": "2015-03-18T04:04:41.837Z"
+})
